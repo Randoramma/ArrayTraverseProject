fadbd13ce4e62c7b85d91bd40e4f04dff670914c
Randy McLain <Randoramma@me.com>
2015-03-01 19:35:36 -0800

    adding to repository
    adding to repository

--- ArrayTraverseProject/ViewController.swift
+++ ArrayTraverseProject/ViewController.swift
@@ -10,16 +10,90 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // constants
+    let ray =  Person(firstName: "Ray", lastName: "Stantz", theStudent: false)
+    let peter = Person(firstName: "Peter", lastName: "Venkman", theStudent: false)
+    let egon = Person(firstName: "Egon", lastName: "Spengler", theStudent: false)
+    let winston = Person(firstName: "Winston", lastName: "Zeddemore", theStudent: false)
+    let louis = Person(firstName: "Louis", lastName: "Tully", theStudent: false)
+    
+    // properties
+    var myIterator = 0
+
+    var myGhostbusters = [Person]()
+
+    
+    // the outlet for the text field connecting it to the storyboard
+    @IBOutlet weak var textOutput: UITextField!
+    
+    @IBOutlet weak var myLeftButton: UIButton!
+    
+    @IBOutlet weak var myRightButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        myLeftButton.hidden = true
+
+        viewWillAppear()
+    }
+    
+    func viewWillAppear() {
+        // Check the status of the iterator.
+       // self.buttonAction()
+        
+        myGhostbusters += [ray, peter, egon, winston, louis]
     }
 
-    override func didReceiveMemoryWarning() {
+
+    func incramentIterator() -> Void {
+        myIterator++
+    }
+    
+    func decramentIterator() -> Void {
+        myIterator--
+    }
+    
+    func textBoxOutputUpdate() -> Void {
+        let theInt = myIterator
+         textOutput.text =  myGhostbusters[theInt].toString()
+        
+    }
+ 
+    
+    func buttonAction() -> Void  {
+        if (myIterator == 0) {
+            myLeftButton.hidden = true
+            
+        
+        } else {
+            myLeftButton.hidden = false
+        }
+        
+        if (myIterator == 4) {
+            myRightButton.hidden = true
+
+        } else {
+            myRightButton.hidden = false
+        }
+
+    }
+    
+    @IBAction func iteration(sender: UIButton) -> Void {
+        if (myLeftButton == sender) {
+            decramentIterator()
+            
+        } else if (myRightButton == sender) {
+            incramentIterator()
+
+    }
+        buttonAction()
+        textBoxOutputUpdate()
+    }
+
+    override func didReceiveMemoryWarning() -> Void {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
